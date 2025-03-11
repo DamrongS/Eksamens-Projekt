0df732a39bb36397b935e1f50c584496d4b52f20
ChristofferS22R <95274166+DamrongS@users.noreply.github.com>
2025-03-11 08:42:16 +0100

    omg

--- remotes.py
+++ remotes.py
@@ -4,6 +4,7 @@ import hashlib
 import server
 
 def login_client(username, password, root):
+    print(server.login_user(username, password))
     if server.login_user(username, password):
         root.destroy()
         return True
